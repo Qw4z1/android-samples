38f436a5ed530599cb25aea0f6dbbe5a2796149f
Viktor Nyblom <viktor.nyblom@gmail.com>
2016-06-20 21:44:41 +0200

    Added null assertion

--- app/src/main/java/se/kicksort/myfirstapp/DialogActivity.java
+++ app/src/main/java/se/kicksort/myfirstapp/DialogActivity.java
@@ -23,6 +23,7 @@ public class DialogActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
